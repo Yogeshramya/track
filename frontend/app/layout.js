cced0ee938451eee22c238c5b768d05763c54a9a
Yogesh_04 <yogeshyogi20052@gmail.com>
2026-08-29 23:55:37 +0530

    feat: add Instagram favicon to layout metadata and public directory

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,14 +1,21 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'mr_in.nocent_yogi',
-  description: 'Instagram Profile.',
+  title: 'Instagram',
+  description: 'Instagram',
+  icons: {
+    icon: 'https://a.favicon.im/instagram.com',
+    shortcut: 'https://a.favicon.im/instagram.com',
+    apple: 'https://a.favicon.im/instagram.com',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" type="image/png" href="https://a.favicon.im/instagram.com" />
+        <link rel="apple-touch-icon" href="https://a.favicon.im/instagram.com" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
